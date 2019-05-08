ab82788acd1e4513d5db5c18763336eb52158a0b
Thomas Rudin <thomas@rudin.io>
2019-05-08 21:16:23 +0200

    cleanup

--- mapgen.lua
+++ mapgen.lua
@@ -1,10 +1,20 @@
 
+
+local has_vacuum_mod = minetest.get_modpath("vacuum")
+
+
 minetest.register_on_generated(function(minp, maxp, seed)
 
 	-- default from 6k to 10k
 	if minp.y < planetoids.miny or minp.y > planetoids.maxy then
 		return
 	end
+
+	if has_vacuum_mod and not vacuum.is_mapgen_block_in_space(minp, maxp) then
+		-- no vacuum there, don't generate planetoids in non-vacuum
+		return
+	end
+
 
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
