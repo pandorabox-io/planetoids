c4e9ca576a74c2dc1c7aed9231fd6268f8c6cece
NatureFreshMilk <naturefreshmilk@github.com>
2019-05-09 14:04:21 +0200

    do only corner check on mapgen

--- mapgen.lua
+++ mapgen.lua
@@ -1,6 +1,28 @@
-
-
 local has_vacuum_mod = minetest.get_modpath("vacuum")
+
+
+local get_corners = function(minp, maxp)
+	return {
+		minp,
+		maxp,
+		{ x=maxp.x, y=minp.y, z=minp.z },
+		{ x=minp.x, y=maxp.y, z=minp.z },
+		{ x=minp.x, y=minp.y, z=maxp.z },
+		{ x=maxp.x, y=maxp.y, z=minp.z },
+		{ x=minp.x, y=maxp.y, z=maxp.z },
+		{ x=maxp.x, y=minp.y, z=maxp.z },
+	}
+end
+
+local check_corners_in_space = function(minp, maxp)
+	for _, pos in ipairs(get_corners(minp, maxp)) do
+		if vacuum.is_pos_in_space(pos) then
+			return true
+		end
+	end
+
+	return false
+end
 
 
 minetest.register_on_generated(function(minp, maxp, seed)
@@ -10,7 +32,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
 		return
 	end
 
-	if has_vacuum_mod and not vacuum.is_mapgen_block_in_space(minp, maxp) then
+	if has_vacuum_mod and not check_corners_in_space(minp, maxp) then
 		-- no vacuum there, don't generate planetoids in non-vacuum
 		return
 	end
