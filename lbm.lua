e4baf4f6c5b236458c27b0a158a6ccf04b2d9569
Thomas Rudin <thomas@rudin.io>
2019-07-08 17:40:38 +0200

    fix loading

--- lbm.lua
+++ lbm.lua
@@ -4,7 +4,7 @@ if minetest.get_modpath("vacuum") then
 	minetest.register_lbm({
 		name = "planetoids:radioactive_stone_replace",
 		nodenames = {"planetoids:radioactive_stone"},
-		-- run_at_every_load = true,
+		run_at_every_load = true,
 		action = function(pos, node)
 			if not vacuum.is_pos_in_space(pos) then
 				node.name = "default:stone"
